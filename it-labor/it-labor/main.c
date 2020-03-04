0a4b72e9794b1b693730ef0fc552df48a5739187
Julian Humplstoetter <sicstar21@gmail.com>
2020-03-04 14:15:50 +0100

    + aufgabe 5

---
+++ it-labor/it-labor/main.c
@@ -0,0 +1,20 @@
+/*
+ * it-labor.c
+ *
+ * Created: 3/4/2020 2:12:38 PM
+ * Author : thesicstar
+ */ 
+#include <avr/io.h>
+
+	DDRA = 0xff;
+	PORTA = 0x01; 
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
